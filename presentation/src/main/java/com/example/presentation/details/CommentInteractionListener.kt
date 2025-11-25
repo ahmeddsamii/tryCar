67a492ac31e74a6820a16d8b44981ca9e17eac1d
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-25 21:06:46 +0200

    feat: Add retry interaction listeners

---
+++ presentation/src/main/java/com/example/presentation/details/CommentInteractionListener.kt
@@ -0,0 +1,5 @@
+package com.example.presentation.details
+
+interface CommentInteractionListener {
+    fun onClickRetry()
+}
--- presentation/src/main/java/com/example/presentation/home/HomeInteractionListener.kt
+++ presentation/src/main/java/com/example/presentation/home/HomeInteractionListener.kt
@@ -2,4 +2,5 @@ package com.example.presentation.home
 
 interface HomeInteractionListener {
     fun onClickPost(postId: Int)
+    fun onClickRetry()
 }
