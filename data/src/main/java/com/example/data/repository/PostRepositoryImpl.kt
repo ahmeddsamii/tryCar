fccf78ec48c9d9618374b9918c9f0f8cd521a16b
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-25 22:07:59 +0200

    feat: Add caching implementation

---
+++ data/src/main/java/com/example/data/repository/PostRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package com.example.data.repository
+
+import com.example.data.local.source.PostLocalDataSource
+import com.example.data.remote.mapper.toDomainPostsFromLocal
+import com.example.data.remote.mapper.toEntityList
+import com.example.data.remote.mapper.toLocal
+import com.example.data.remote.source.PostRemoteDataSource
+import com.example.domain.entity.Comment
+import com.example.domain.entity.Post
+import com.example.domain.repository.PostRepository
+import org.koin.core.annotation.Single
+
+@Single(binds = [PostRepository::class])
+class PostRepositoryImpl(
+    private val remoteDataSource: PostRemoteDataSource,
+    private val localDataSource: PostLocalDataSource
+) : PostRepository {
+    override suspend fun getAllPosts(): List<Post> {
+
+        return runCatching {
+            val remotePosts = remoteDataSource.getAllPosts().toEntityList()
+
+            localDataSource.clearAllPosts()
+            localDataSource.insertAllPosts(remotePosts.toLocal())
+
+            remotePosts
+        }.getOrElse {
+            localDataSource.getAllPosts().toDomainPostsFromLocal()
+        }
+    }
+
+    override suspend fun getAllCommentsByPostId(postId: Int): List<Comment> {
+        return remoteDataSource.getAllCommentsByPostId(postId).toEntityList()
+    }
+}
