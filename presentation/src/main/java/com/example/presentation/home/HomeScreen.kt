fb3a61c60eb8a44182306cfb805ad7a67caff0e3
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-25 19:31:52 +0200

    feat: Add post list UI in HomeScreen

--- presentation/src/main/java/com/example/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/example/presentation/home/HomeScreen.kt
@@ -1,12 +1,22 @@
 package com.example.presentation.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.androidx.compose.koinViewModel
 
@@ -14,7 +24,45 @@ import org.koin.androidx.compose.koinViewModel
 fun HomeScreen(viewModel: HomeViewModel = koinViewModel()) {
 
     val state by viewModel.state.collectAsStateWithLifecycle()
-    Box(Modifier.fillMaxSize()){
-        Text(text = state.posts.firstOrNull()?.id.toString(), modifier = Modifier.align(Alignment.Center))
+    HomeScreenContent(state)
+}
+
+@Composable
+fun HomeScreenContent(
+    state: HomeUiState
+) {
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(vertical = 16.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(state.posts) { post ->
+            PostItem(title = post.title, body = post.body)
+        }
+    }
+}
+
+@Composable
+private fun PostItem(
+    title: String,
+    body: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(100.dp)
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
+        Text(text = body)
     }
 }
