35795a693619474767182edc49ca71aedc42349b
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-25 23:27:01 +0200

    feat: Add favorite screen

---
+++ presentation/src/main/java/com/example/presentation/favorite/FavoriteScreen.kt
@@ -0,0 +1,15 @@
+package com.example.presentation.favorite
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoriteScreen() {
+    Box(Modifier.fillMaxSize()) {
+        Text(text = "Favorite", modifier = Modifier.align(Alignment.Center))
+    }
+}
