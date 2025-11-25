c1b9040282b84bbec9ae19bf10919ac0e35a8061
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-25 19:32:08 +0200

    feat: Wrap MainGraph with TrycarTheme

--- app/src/main/java/com/example/try_car/MainActivity.kt
+++ app/src/main/java/com/example/try_car/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.try_car
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.try_car.navigation.MainGraph
+import com.example.try_car.ui.theme.TrycarTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainGraph()
+            TrycarTheme {
+                MainGraph()
+            }
         }
     }
 }
