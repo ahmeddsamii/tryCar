bdee8bdf52c3f8715d2a355a1db8d22087191329
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-26 10:19:36 +0200

    feat: Add FavoritePost entity to AppDatabase

--- data/src/main/java/com/example/data/local/database/AppDatabase.kt
+++ data/src/main/java/com/example/data/local/database/AppDatabase.kt
@@ -3,9 +3,10 @@ package com.example.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.data.local.dao.PostDao
+import com.example.data.local.entity.FavoritePost
 import com.example.data.local.entity.LocalPost
 
-@Database(entities = [LocalPost::class], version = 1)
+@Database(entities = [LocalPost::class, FavoritePost::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun postDao(): PostDao
 }
