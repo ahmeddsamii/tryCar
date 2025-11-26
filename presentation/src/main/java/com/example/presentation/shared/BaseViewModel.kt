40314916028501f105c75193a2afb96d245eccb7
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-26 14:27:03 +0200

    refactor: Remove tryToCollectFlow from BaseViewModel

--- presentation/src/main/java/com/example/presentation/shared/BaseViewModel.kt
+++ presentation/src/main/java/com/example/presentation/shared/BaseViewModel.kt
@@ -10,17 +10,10 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.coroutines.cancellation.CancellationException
@@ -68,34 +61,6 @@ abstract class BaseViewModel<State, Effect>(
                     mapExceptionToErrorState(throwable, onError)
                 }
             onEnd()
-        }
-    }
-
-    protected fun <R> tryToCollectFlow(
-        block: () -> Flow<R>,
-        onStart: () -> Unit = {},
-        onNewValue: (R) -> Unit,
-        onError: (ErrorState) -> Unit,
-        onEnd: () -> Unit = {},
-        dispatcher: CoroutineDispatcher = Dispatchers.IO,
-        scope: CoroutineScope = viewModelScope
-    ): Job {
-        val exceptionHandler = CoroutineExceptionHandler { _, exception ->
-            onError(ErrorState.RequestFailed(exception.message))
-        }
-
-        return scope.launch(dispatcher + exceptionHandler) {
-            block()
-                .flowOn(dispatcher)
-                .onStart { onStart() }
-                .onEach { onNewValue(it) }
-                .onCompletion { throwable ->
-                    throwable?.let {
-                        mapExceptionToErrorState(throwable, onError)
-                    } ?: onEnd()
-                }
-                .catch { throwable -> mapExceptionToErrorState(throwable, onError) }
-                .collect()
         }
     }
 
