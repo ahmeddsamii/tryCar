71c5831d7b39d59596d3fcaf23214e28b27c48ba
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-27 08:00:24 +0200

    refactor: Move NoConnection composable to component package

--- presentation/src/main/java/com/example/presentation/shared/component/NoConnection.kt
+++ presentation/src/main/java/com/example/presentation/shared/component/NoConnection.kt
@@ -1,4 +1,4 @@
-package com.example.presentation.shared
+package com.example.presentation.shared.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
